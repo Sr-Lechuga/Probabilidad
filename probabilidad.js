87b0658baac5837949827ace5b2c80efe3ec2959
Sr.Lechuga <jonattanlima@gmail.com>
2023-05-17 22:16:00 -0300

    Test with a 10000 numbers, simple comments

--- probabilidad.js
+++ probabilidad.js
@@ -2,8 +2,7 @@ function calculateProbabilities(integerArray) {
     /* Probability is all favorable cases divided all possible cases, to make it percentual gets mutiplicated by 100 
 
         Precondition: Arreglo de enteros (pueden haber repetidos) 
-    
-       */
+    */
     const PROBABILITY_VALUE = 1;
 
     const totalCases = integerArray.length;
@@ -16,7 +15,6 @@ function calculateProbabilities(integerArray) {
         if none is defined for a number, starts at 1.*/
     integerArray.forEach(number => (repetitions[number] = repetitions[number] + 1 || 1));
     
-    /* Define a map, with probability for each number */
     const probabilities = new Map();
     Object.keys(repetitions).forEach(number => {
         const favorableCases = repetitions[number];
@@ -28,13 +26,13 @@ function calculateProbabilities(integerArray) {
     /* Probability is in the second position of the array in each map entry, so it's acceces with const variable PROBABILITY_VALUE */
     const sortedProbabilities = new Map([...probabilities.entries()].sort((j, l) => j[PROBABILITY_VALUE] - l[PROBABILITY_VALUE]));
     
-    /* Key value for map object represent each number (without repeat them)  */
     return Array.from(sortedProbabilities.keys());
 }
 
-// Generate 100 random numbers to test
+//Test
 const numeros = [];
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 10000; i++) {
+    //Random numbers from 1 to 100
     numeros.push(Math.floor(Math.random() * 101));
 }
 
